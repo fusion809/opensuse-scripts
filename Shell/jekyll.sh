539e32838a9cc7e50284bc6ba3e26a25d6d52baa
fusion809 <brentonhorne77@gmail.com>
2016-02-13 18:59:44 +1000

    Updating scripts

--- Shell/jekyll.sh
+++ Shell/jekyll.sh
@@ -7,6 +7,13 @@ function jekex {
   bundle exec jekyll serve -I --future $@
 }
 
+function lastmod {
+  prevmod=$(sed -n 's/last_modified_at:\s*//p' $1)
+  lastmod=$(date -r $1 +'%Y-%m-%d %H:%M:%S %z')
+  echo "prevmod is $prevmod\nlastmod is $lastmod"
+  sed -i -e "s/last_modified_at: $prevmod/last_modified_at: $lastmod/g" $1
+}
+
 function wgetg {
 	wget -c https://gist.github.com/$1
 }
