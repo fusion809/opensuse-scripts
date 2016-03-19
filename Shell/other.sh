873c68df19bfe8e04bdd52b0c4d220c46196c480
fusion809 <brentonhorne77@gmail.com>
2016-03-20 07:37:09 +1000

    Updating scripts

--- Shell/other.sh
+++ Shell/other.sh
@@ -1,4 +1,4 @@
 function xadj {
 	xrandr --output VGA-0 --mode 1366x$1
 }
-xadj 644 || xadj 669 || xadj 671 || xadj 672 || xadj 654 || xadj 664
+#xadj 644 || xadj 669 || xadj 671 || xadj 672 || xadj 654 || xadj 664
