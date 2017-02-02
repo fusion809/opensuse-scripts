c142ac56010436bf772d6bbfd1363878b09f6fa2
fusion809 <brentonhorne77@gmail.com>
2017-02-02 19:26:22 +1000

    Updating scripts

--- Shell/git.sh
+++ Shell/git.sh
@@ -93,7 +93,7 @@ function pushss {
 # opensuse-scripts
   ## Update opensuse-scripts GitHub repo
   function shup {
-    rm -rf $OS/{Shell,root,.zshrc,.bashrc} && mkdir -p $OS/{Shell,root/Shell} && cps && cdos && push "$1" && szsh && cd -
+    rm -rf $OS/{Shell,root,.zshrc,.bashrc} && mkdir -p $OS/{Shell,root/Shell} && chmod +x -R $HOME/Shell && sudo chmod +x -R /root/Shell && cps && cdos && push "$1" && szsh && cd -
 		if [[ $SHELL == /bin/zsh ]] || [[ $SHELL = /usr/bin/zsh ]]; then
 			szsh
 		elif [[ $SHELL == /bin/bash ]] || [[ $SHELL == /usr/bin/bash ]]; then
