ec0204e0250cd0ac62f7508da6e6922329b94f6b
fusion809 <brentonhorne77@gmail.com>
2017-02-06 07:15:14 +1000

    Adjusting cpv function

--- Shell/git/05-vim.sh
+++ Shell/git/05-vim.sh
@@ -1,7 +1,9 @@
 # Update vim local repo
 
 function cpv {
-	cp $HOME/.vimrc $EDT/vim && cp $HOME/.vim/syntax/sh.vim $EDT/vim
+	cp $HOME/.vimrc $EDT/vim
+        cp $HOME/.vim/syntax/sh.vim $EDT/vim
+        cp $HOME/.vim/spell/en.utf-8.add $EDT/vim
 }
 
 function shvup {
