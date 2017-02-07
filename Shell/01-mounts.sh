07e89ef119c08c1a5e027f9e7cdf087856ab4509
fusion809 <brentonhorne77@gmail.com>
2017-02-08 03:07:46 +1000

    Updating scripts

--- Shell/01-mounts.sh
+++ Shell/01-mounts.sh
@@ -1,13 +1,17 @@
-# Mount Fedora
-if ! `grep -qs "/mnt" /proc/mounts`; then
-  sudo mount /dev/sdb2 /mnt
-  sudo mount -t proc proc /mnt/proc
-  sudo mount --rbind /dev /mnt/dev
-  sudo mount --make-rslave /mnt/dev
-  sudo mount --rbind /sys /mnt/sys
-  sudo mount --make-rslave /mnt/sys
-  sudo mount --rbind /tmp /mnt/tmp
-  sudo cp -L /etc/resolv.conf /mnt/etc
-else
-  printf "/dev/sdb3 is mounted on /mnt\n"
-fi
+# Mount Manjaro
+function mmanj {
+	if ! `grep -qs "/mnt" /proc/mounts`; then
+	  sudo mount /dev/sdb2 /mnt
+	  sudo mount -t proc proc /mnt/proc
+	  sudo mount --rbind /dev /mnt/dev
+	  sudo mount --make-rslave /mnt/dev
+	  sudo mount --rbind /sys /mnt/sys
+	  sudo mount --make-rslave /mnt/sys
+	  sudo mount --rbind /tmp /mnt/tmp
+	  sudo cp -L /etc/resolv.conf /mnt/etc
+	else
+	  printf "/dev/sdb3 is mounted on /mnt\n"
+	fi
+}
+
+mmanj
