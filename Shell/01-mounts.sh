170a96508111874dd21ed7c67cec877c29bd2f99
fusion809 <brentonhorne77@gmail.com>
2017-02-09 00:05:01 +1000

    Updating scripts

--- Shell/01-mounts.sh
+++ Shell/01-mounts.sh
@@ -10,8 +10,14 @@ function mmanj {
 	  sudo mount --rbind /tmp /mnt/tmp
 	  sudo cp -L /etc/resolv.conf /mnt/etc
 	else
-	  printf "/dev/sdb3 is mounted on /mnt\n"
+	  printf "/dev/sdb2 is mounted on /mnt\n"
 	fi
 }
 
 mmanj
+
+if ! `grep -qs "/data" /proc/mounts`; then
+	sudo mount /dev/sdb4 /data
+else
+	printf "/dev/sdb4 is mounted on /data\n"
+fi
