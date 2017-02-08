c28af3fd9628fcefc364fe62b6b0feae60bd0331
fusion809 <brentonhorne77@gmail.com>
2017-02-09 02:35:58 +1000

    Updating scripts

--- Shell/programs/03-docker.sh
+++ Shell/programs/03-docker.sh
@@ -2,7 +2,7 @@ if ! [[ -f /usr/bin/docker ]]; then
 	zypar \
 	    https://yum.dockerproject.org/repo/main/opensuse/13.2/ \
 	    docker-main
-	zypin docker
+	zypin docker-engine
 	sudo gpasswd -a $USER docker
 	sudo systemctl enable docker
 	sudo systemctl start docker
