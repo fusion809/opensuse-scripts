c50e5ae0bf137e55c66028293c2e25ab41f0e63d
fusion809 <brentonhorne77@gmail.com>
2017-02-09 10:02:05 +1000

    Updating scripts

--- Shell/programs/nonvm/11-runescape.sh
+++ Shell/programs/nonvm/11-runescape.sh
@@ -1,15 +1,15 @@
 if ! [[ -d $PKG/runescape-launcher ]]; then
 	gitc runescape-launcher $PKG/runescape-launcher
-fi
 
-if ! [[ -d /usr/local/bin ]]; then
-	sudo mkdir -p /usr/local/bin
-fi
+	if ! [[ -d /usr/local/bin ]]; then
+		sudo mkdir -p /usr/local/bin
+	fi
 
-if ! [[ -d /usr/local/share/applications ]]; then
-	sudo mkdir -p /usr/local/share/applications
-fi
+	if ! [[ -d /usr/local/share/applications ]]; then
+		sudo mkdir -p /usr/local/share/applications
+	fi
 
-sudo cp $PKG/runescape-launcher/runescape-launcher /usr/local/bin
-sudo cp $PKG/runescape-launcher/runescape-launcher.desktop /usr/local/share/applications
-docker pull fusion809/runescape-launcher:latest
+	sudo cp $PKG/runescape-launcher/runescape-launcher /usr/local/bin
+	sudo cp $PKG/runescape-launcher/runescape-launcher.desktop /usr/local/share/applications
+	docker pull fusion809/runescape-launcher:latest
+fi
