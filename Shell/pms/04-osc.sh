b799004698220dc236b1e315a6cc814a8a740b50
fusion809 <brentonhorne77@gmail.com>
2017-02-20 06:24:30 +1000

    Updating scripts

--- Shell/pms/04-osc.sh
+++ Shell/pms/04-osc.sh
@@ -22,10 +22,11 @@ function ovimup {
   vim_patchversion=$(cat vim.spec | grep "%define.*patchlevel" | sed 's/%define.*patchlevel\s*//g' | head -n 1)
   if [[ $baseversion != $vim_baseversion ]]; then
     sed -i -e "s|baseversion $vim_baseversion|baseversion $baseversion|g" vim.spec
-    sed -i -e "s|$vim_baseversion|$baseversion|g" {$HOME/AUR/gvim-gtk2/PKGBUILD,$HOME/OBS/home:fusion809/gvim-gtk2/PKGBUILD}
+    sed -i -e "s|$vim_baseversion|$baseversion|g" $HOME/OBS/home:fusion809/gvim-gtk2/PKGBUILD
     sed -i -e 's|Release:       [0-9].*|Release:       1|g' vim.spec
     if [[ "$1" == "vim" ]]; then
       sed -i -e "s|$vim_baseversion|$baseversion|g" $HOME/AUR/gvim-gtk2/PKGBUILD
+      sed -i -e "s|$vim_baseversion|$baseversion|g" $PKG/PKGBUILDs/gvim-{gtk2,gtk3}/PKGBUILD
     fi
   fi
   if [[ $patchversion != $vim_patchversion ]]; then
@@ -35,10 +36,18 @@ function ovimup {
     fi
     sed -i -e 's|Release:       [0-9].*|Release:       1|g' vim.spec
     if [[ "$1" == "vim" ]]; then
-      sed -i -e "s|$vim_patchversion|$patchversion|g" $HOME/AUR/gvim-gtk2/PKGBUILD
-      sed -i -e "s|$vim_patchversion|$patchversion|g" $HOME/OBS/home:fusion809/gvim-gtk2/PKGBUILD
+      sed -i -e "s|$vim_patchversion|$patchversion|g" {$HOME/OBS/home:fusion809/gvim-gtk2,$HOME/AUR/gvim-gtk2,$PKG/PKGBUILDs/gvim-gtk2,$PKG/PKGBUILDs/gvim-gtk3}/PKGBUILD
       cd $HOME/AUR/gvim-gtk2
       push "Bumping to $pkgver"
+      cd -
+      cd $PKG/PKGBUILDs/gvim-gtk2
+      push "Bumping to $pkgver"
+      cd -
+      cd $PKG/PKGBUILDs/gvim-gtk3
+      push "Bumping to $pkgver"
+      cd -
+      cd $PKG/PKGBUILDs
+      push "Bumping gvim submodules to $pkgver"
       cd -
       cd $HOME/OBS/home:fusion809/gvim-gtk2
       osc ci -m "Bumping to $pkgver"
