5cb161aedb48c08496632213fd299d1709a6f770
Brenton Horne <brentonhorne77@gmail.com>
2017-02-21 01:39:03 +1000

    Create 03-broadcom.sh

---
+++ Shell/programs/nonvm/03-broadcom.sh
@@ -0,0 +1,8 @@
+if `/sbin/lspci | grep Broadcom >/dev/null 2>&1`; then
+  BROADOUT=$(rpm -qi broadcom-wl)
+  if [[ $BROADOUT == "package broadcom-wl is not installed" ]]; then
+    sudo zypper ar -f -n packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+  fi
+else
+  printf "$HOME/Shell/programs/nonvm/03-broadcom.sh has detected that this PC does not required broadcom-wl\n"
+fi
