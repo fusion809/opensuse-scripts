7a26f75cfd26fb6962da70c189680476fd4b605d
Brenton Horne <brentonhorne77@gmail.com>
2017-02-21 01:41:21 +1000

    Update 03-broadcom.sh

--- Shell/programs/nonvm/03-broadcom.sh
+++ Shell/programs/nonvm/03-broadcom.sh
@@ -4,6 +4,7 @@ if `/sbin/lspci | grep Broadcom >/dev/null 2>&1`; then
     sudo zypper ar -f -n packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
     sudo zypper refresh
     zpi broadcom-wl
+    sudo modprobe -a wl
   fi
 else
   printf "$HOME/Shell/programs/nonvm/03-broadcom.sh has detected that this PC does not required broadcom-wl\n"
