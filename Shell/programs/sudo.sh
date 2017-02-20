3bae3e922f7c8edd0fabe8f7858b4b16a552b015
fusion809 <brentonhorne77@gmail.com>
2017-02-21 02:10:09 +1000

    Adding sudo script to set up sudo without pass

--- Shell/programs/sudo.sh
+++ Shell/programs/sudo.sh
@@ -1,4 +1,4 @@
-if `sudo cat /etc/sudoers | grep %wheel | grep "^#" > /dev/null >2&1`; then
+if `sudo cat /etc/sudoers | grep %wheel | grep "^#" >/dev/null 2>&1`; then
   sudo sed -i -e "s/^#.*%wheel/%wheel/g" /etc/sudoers
   sudo gpasswd -a fusion809 wheel
 fi
