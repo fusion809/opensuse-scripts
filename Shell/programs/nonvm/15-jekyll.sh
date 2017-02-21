00159d7f88d028ecf2f9861694d6a82f52d4eb56
fusion809 <brentonhorne77@gmail.com>
2017-02-21 22:45:11 +1000

    Updating scripts

--- Shell/programs/nonvm/15-jekyll.sh
+++ Shell/programs/nonvm/15-jekyll.sh
@@ -1,19 +1,15 @@
 cdfgi
 
 # Check if the necessary packages are installed
-RUBC=$(rpm -qi ruby-devel)
-RUBG=$(rpm -qi rubygems)
-ZLD=$(rpm -qi zlib-devel)
-
-if [[ "$RUBC" == "package ruby-devel is not installed" ]]; then
+if ! [[ -d /usr/share/doc/packages/ruby-devel ]]; then
 	zpi ruby-devel
 fi
 
-if [[ "$RUBG" == "package rubygems is not installed" ]]; then
+if ! [[ -d /usr/share/doc/packages/ruby ]]; then
 	zpi rubygems
 fi
 
-if [[ "$ZLD" == "package zlib-devel is not installed" ]]; then
+if ! [[ -d /usr/share/doc/packages/zlib-devel ]]; then
 	zpi zlib-devel
 fi
 
