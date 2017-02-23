1a5b79b8986e02526736ef1abf1e8c5a7a6398a6
fusion809 <brentonhorne77@gmail.com>
2017-02-23 21:42:28 +1000

    Updating scripts; esp syms

--- Shell/04-repos.sh
+++ Shell/04-repos.sh
@@ -6,14 +6,14 @@ function gitc {
 # vim
 if `cat /proc/cpuinfo | grep hypervisor >/dev/null 2>&1`; then
 	if ! [[ -d $EDT/vim ]]; then
-       git clone https://github.com/fusion809/vim $EDT/vim
-       mkdir -p $HOME/.vim/{autoload,bundle,colors,plugins,spell,syntax}
-       cp $EDT/vim/.vimrc $HOME
-       cp $EDT/vim/*.add $HOME/.vim/spell
-       cp $EDT/vim/sh.vim $HOME/.vim/syntax
-       curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-       git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-       wget -cq https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O $HOME/.vim/colors/molokai.vim
+		git clone https://github.com/fusion809/vim $EDT/vim
+		mkdir -p $HOME/.vim/{autoload,bundle,colors,plugins,spell,syntax}
+		cp $EDT/vim/.vimrc $HOME
+		cp $EDT/vim/*.add $HOME/.vim/spell
+		cp $EDT/vim/sh.vim $HOME/.vim/syntax
+		curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+		git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+		wget -cq https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O $HOME/.vim/colors/molokai.vim
 	fi
 else
 	function sym {
