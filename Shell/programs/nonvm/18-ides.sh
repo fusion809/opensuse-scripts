a7106a77dca1b2aa21df2dd3e9680e5dd638b0da
fusion809 <brentonhorne77@gmail.com>
2017-02-23 23:36:52 +1000

    Updating scripts

--- Shell/programs/nonvm/18-ides.sh
+++ Shell/programs/nonvm/18-ides.sh
@@ -1,3 +1,3 @@
 if ! [[ -f /usr/bin/codeblocks ]]; then
-	zpi bluefish codeblocks codelite eclipse geany gnome-builder kdevelop5 komodo-edit netbeans
+	zpi bluefish codeblocks codelite eclipse geany gnome-builder kdevelop5 komodo-edit monodevelop netbeans
 fi
