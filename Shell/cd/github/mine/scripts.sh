17f06cad199c96e95fa7b58b05b37ddb799939d0
fusion809 <brentonhorne77@gmail.com>
2017-02-25 01:39:12 +1000

    Updating scripts

--- Shell/cd/github/mine/scripts.sh
+++ Shell/cd/github/mine/scripts.sh
@@ -2,18 +2,6 @@ function cdsc {
 	cd "$SCR/$1"
 }
 
-function cdgn {
-	cdsc "GNU_Octave/$1"
-}
-
-function cdjs {
-	cdsc "JScripts/$1"
-}
-
-function cdpy {
-	cdsc "python-scripts/$1"
-}
-
 function cdds {
 	cdsc "debian-scripts/$1"
 }
@@ -22,6 +10,26 @@ function cdfs {
 	cdsc "fedora-scripts/$1"
 }
 
+function cdgn {
+	cdsc "GNU_Octave/$1"
+}
+
+function cdjs {
+	cdsc "JScripts/$1"
+}
+
 function cdos {
 	cdsc "opensuse-scripts/$1"
+}
+
+function cdpy {
+	cdsc "python-scripts/$1"
+}
+
+function cdscs {
+	cdsc "scilab-scripts/$1"
+}
+
+function cdzt {
+	cdsc "zsh-theme/$1"
 }
