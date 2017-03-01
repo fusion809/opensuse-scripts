11d984d81f5ca68735a560fe52ad109c5627b02b
Brenton Horne <brentonhorne77@gmail.com>
2017-03-01 19:23:34 +1000

    Update quick-deploy.sh

--- quick-deploy.sh
+++ quick-deploy.sh
@@ -43,6 +43,11 @@ chsh -s /bin/zsh
 # Set root shell to Zsh
 sudo chsh -s /bin/zsh
 
+for i in $HOME/Shell/*.sh
+do
+	. "$i"
+done
+
 # Test if running in VM - test is from http://unix.stackexchange.com/questions/3685/find-out-if-the-os-is-running-in-a-virtual-environment
 if ! `cat /proc/cpuinfo | grep hypervisor >/dev/null 2>&1`; then
 	for i in $HOME/Shell/programs/nonvm/*.sh
