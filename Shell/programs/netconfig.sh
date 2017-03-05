f8afc2df2d98cd93401f24923dd0651d9d8fe5c5
Brenton Horne <brentonhorne77@gmail.com>
2017-03-06 07:18:34 +1000

    Hopefully getting network resolv.conf to be fixed on startup

--- Shell/programs/netconfig.sh
+++ Shell/programs/netconfig.sh
@@ -1,3 +1,8 @@
 function netc {
 	sudo netconfig -f update
 }
+
+case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
+  [23]) printf "HTTP connectivity is up\n";;
+  *) netc && printf "The net should be working now\n";;
+esac
