d2443a63e737606b4a6fa6890fb9fe7f49734bb1
Brenton Horne <brentonhorne77@gmail.com>
2017-03-06 07:21:13 +1000

    Updating scripts

--- Shell/programs/netconfig.sh
+++ Shell/programs/netconfig.sh
@@ -3,6 +3,6 @@ function netc {
 }
 
 case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
-  [23]) printf "HTTP connectivity is up\n";;
+  [23]) printf "Networking seems to be working fine...\n";;
   *) netc && printf "The net should be working now\n";;
 esac
