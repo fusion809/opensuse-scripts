a2ba516a6377bc85659e0da8e68499876649ebad
Brenton Horne <brentonhorne77@gmail.com>
2017-03-14 09:07:39 +1000

    Adding mfunt

--- Shell/01-mounts.sh
+++ Shell/01-mounts.sh
@@ -13,6 +13,20 @@ function mmanj {
 
 mmanj
 
+function mfunt {
+	if ! `grep -qs "/funtoo/proc" /proc/mounts`; then
+		sudo mount -t proc proc /funtoo/proc
+		sudo mount --rbind /dev /funtoo/dev
+		sudo mount --make-rslave /funtoo/dev
+		sudo mount --rbind /sys /funtoo/sys
+		sudo mount --make-rslave /funtoo/sys
+		sudo mount --rbind /tmp /funtoo/tmp
+		sudo cp -L /etc/resolv.conf /funtoo/etc
+	fi
+}
+
+mfunt
+
 if ! [[ -d /data ]]; then
 	sudo mkdir -p /data
 fi
