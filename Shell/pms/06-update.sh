df326c10688076d2942fd9ecb32028b4c0475513
fusion809 <brentonhorne77@gmail.com>
2017-12-21 00:01:20 +1000

    Updating

--- Shell/pms/06-update.sh
+++ Shell/pms/06-update.sh
@@ -5,10 +5,9 @@ function update {
 	printf '\e[1;34m%-0s\e[m' "Updating Flatpaks..."
 	printf "\n"
 	flatup
-	printf '\e[1;34m%-0s\e[m' "Running aptup in Ubuntu chroot..."
-	printf "\n"
-	aptup
 	printf '\e[1;34m%-0s\e[m' "Updating Atom packages and themes..."
 	printf "\n"
-	apmup
+	if `which apm > /dev/null 2>&1`; then
+              apmup
+        fi
 }
