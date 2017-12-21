f4cc228377ad478e00389679dd0c78100c01104e
fusion809 <brentonhorne77@gmail.com>
2017-12-21 23:46:52 +1000

    Updating

--- Shell/pms/06-update.sh
+++ Shell/pms/06-update.sh
@@ -1,13 +1,15 @@
 function update {
-	printf '\e[1;34m%-0s\e[m' "Running zypper update..."
-	printf "\n"
-	zypup
-	printf '\e[1;34m%-0s\e[m' "Updating Flatpaks..."
-	printf "\n"
-	flatup
-	printf '\e[1;34m%-0s\e[m' "Updating Atom packages and themes..."
-	printf "\n"
-	if `which apm > /dev/null 2>&1`; then
-              apmup
-        fi
+    printf '\e[1;34m%-0s\e[m' "Running zypper update..."
+    printf "\n"
+    zypup
+    printf '\e[1;34m%-0s\e[m' "Updating Flatpaks..."
+    printf "\n"
+    if `which flatpak > /dev/null 2>&1`; then
+         flatup
+    fi
+    printf '\e[1;34m%-0s\e[m' "Updating Atom packages and themes..."
+    printf "\n"
+    if `which apm > /dev/null 2>&1`; then
+         apmup
+    fi
 }
