c6a303b051e0badad131c492067ba2b7028a77d0
fusion809 <brentonhorne77@gmail.com>
2018-05-10 13:23:00 +1000

    Adding 0adup func

--- Shell/01-common.sh
+++ Shell/01-common.sh
@@ -2,3 +2,5 @@ for i in $HOME/Shell/common-scripts/*.sh
 do
     . $i
 done
+
+printf "You may want to run check-updates!\n"
--- Shell/pms/04-osc.sh
+++ Shell/pms/04-osc.sh
@@ -143,3 +143,23 @@ function mobc {
 	osc mkpac "$1"
 	obc "$1"
 }
+
+function 0adup {
+    cdgo 0ad
+    git pull origin master -q
+    mastn=$(git rev-list --branches master --count)
+    specn=$(cat $HOME/OBS/home:fusion809/0ad/0ad.spec | grep "Version:" | sed 's/Version:\s*//g')
+    comm=$(git log | head -n 1 | cut -d ' ' -f 2)
+    specm=$(cat $HOME/OBS/home:fusion809/0ad/0ad.spec | grep "define commit" | cut -d ' ' -f 3)
+
+    if [[ $specn == $mastn ]]; then
+         printf "0 A.D. is up to date!"
+    else
+         sed -i -e "s/$specn/$mastn/g" $HOME/OBS/home:fusion809/0ad/0ad.spec
+         sed -i -e "s/$specn/$mastn/g" $HOME/OBS/home:fusion809/0ad-data/0ad-data.spec
+         sed -i -e "s/$specm/$comm/g" $HOME/OBS/home:fusion809/0ad/0ad.spec
+         sed -i -e "s/$specm/$comm/g" $HOME/OBS/home:fusion809/0ad-data/0ad-data.spec
+         cdobsh 0ad; osc ci -m "Bumping $specn->$mastn"
+         cdobsh 0ad-data; osc ci -m "Bumping $specn->$mastn"
+    fi
+}
