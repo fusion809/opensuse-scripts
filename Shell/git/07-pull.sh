5a40c85a0f7a8ca36b1a37a274ac18c97b7c1ce3
fusion809 <brentonhorne77@gmail.com>
2018-05-19 06:07:38 +1000

    Adding gitup func

--- Shell/git/07-pull.sh
+++ Shell/git/07-pull.sh
@@ -1,3 +1,14 @@
 function pullob {
 	git pull origin $(git-branch)
 }
+
+function gitup {
+    cdgo
+    for i in *
+    do
+         pushd $i
+         git stash -q
+         git pull --all -q
+         popd
+    done
+}
