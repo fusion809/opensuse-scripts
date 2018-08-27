730fc8890d4adc5e4db245d0fc669faeb9e7bb23
Brenton Horne <brentonhorne77@gmail.com>
2018-08-28 04:29:51 +1000

    [pms/06-update.sh] tab indent, snap ref

--- Shell/pms/06-update.sh
+++ Shell/pms/06-update.sh
@@ -1,20 +1,21 @@
 function update {
-    printf '\e[1;34m%-0s\e[m' "Running zypper update..."
-    printf "\n"
-    zypup
-    printf '\e[1;34m%-0s\e[m' "Updating Flatpaks..."
-    printf "\n"
-    if `which flatpak > /dev/null 2>&1`; then
-         flatup
-    fi
-    printf '\e[1;34m%-0s\e[m' "Updating Atom packages and themes..."
-    printf "\n"
-    if `which apm > /dev/null 2>&1`; then
-         apmup
-    fi
-    latest-openra-tried=$(ls $HOME/.openra/maps/ra | grep bleed- | tail -n 1)
-    latest-installed=$(cat /usr/lib/openra/VERSION)
-    if [[ ${latest-installed} == ${latest-openra-tried} ]]; then
-         mv $HOME/.openra/maps/ra/${latest-openra-tried}/* $HOME/.openra/maps/ra/${latest-installed}
-    fi
+	printf '\e[1;34m%-0s\e[m' "Running zypper update..."
+	printf "\n"
+	zypup
+	printf '\e[1;34m%-0s\e[m' "Updating Flatpaks..."
+	printf "\n"
+	if `which flatpak > /dev/null 2>&1`; then
+		 flatup
+	fi
+	printf '\e[1;34m%-0s\e[m' "Updating Atom packages and themes..."
+	printf "\n"
+	if `which apm > /dev/null 2>&1`; then
+		 apmup
+	fi
+	latest-openra-tried=$(ls $HOME/.openra/maps/ra | grep bleed- | tail -n 1)
+	latest-installed=$(cat /usr/lib/openra/VERSION)
+	if [[ ${latest-installed} == ${latest-openra-tried} ]]; then
+		 mv $HOME/.openra/maps/ra/${latest-openra-tried}/* $HOME/.openra/maps/ra/${latest-installed}
+	fi
+	sudo snap refresh
 }
