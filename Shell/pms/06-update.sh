57a2fd3a796713f2bb0389cac5aa1f06c6d036c6
Brenton Horne <brentonhorne77@gmail.com>
2018-09-21 00:04:53 +1000

    Expanding update func

--- Shell/pms/06-update.sh
+++ Shell/pms/06-update.sh
@@ -2,20 +2,29 @@ function update {
 	printf '\e[1;34m%-0s\e[m' "Running zypper ref + dup..."
 	printf "\n"
 	dup
-	printf '\e[1;34m%-0s\e[m' "Updating Flatpaks..."
-	printf "\n"
 	if `which flatpak > /dev/null 2>&1`; then
-		 flatup
+		printf '\e[1;34m%-0s\e[m' "Updating Flatpaks..."
+		printf "\n"
+		flatup
 	fi
-	printf '\e[1;34m%-0s\e[m' "Updating Atom packages and themes..."
-	printf "\n"
 	if `which apm > /dev/null 2>&1`; then
-		 apmup
+		printf '\e[1;34m%-0s\e[m' "Updating Atom packages and themes..."
+		printf "\n"
+		apmup
 	fi
 	latestopenratried=$(ls $HOME/.openra/maps/ra | grep bleed- | tail -n 1)
 	latestinstalled=$(cat /usr/lib/openra/VERSION)
 	if [[ ${latestinstalled} == ${latestopenratried} ]]; then
 		 mv $HOME/.openra/maps/ra/${latestopenratried}/* $HOME/.openra/maps/ra/${latestinstalled}
 	fi
-	sudo snap refresh
+	if which snap > /dev/null 2>&1; then
+		printf '\e[1;34m%-0s\e[m' "Updating Snap packages..."
+		printf "\n"
+		sudo snap refresh
+	fi
+	if which nix > /dev/null 2>&1; then
+		printf '\e[1;34m%-0s\e[m' "Updating Nix packages..."
+		printf "\n"
+		nixup
+	fi
 }
