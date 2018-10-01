9ebba11122326fe46cbbe6d8b80b4d5838e6d17e
Brenton Horne <brentonhorne77@gmail.com>
2018-10-01 15:37:14 +1000

    Fixing GUIX var def

--- root/Shell/variables.sh
+++ root/Shell/variables.sh
@@ -1,3 +1,5 @@
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games:/usr/sbin/:/sbin
-export GUIX_PROFILE="`echo ~`/.guix-profile" ; \
-source $GUIX_PROFILE/etc/profile
+if [[ -d ~/.guix-profile ]]; then
+	export GUIX_PROFILE="`echo ~`/.guix-profile" ; \
+	source $GUIX_PROFILE/etc/profile
+fi
